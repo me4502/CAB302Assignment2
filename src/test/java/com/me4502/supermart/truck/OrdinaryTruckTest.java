b83c660502bd4a755b5197229dacb602d96c6501
Liam Dale <liamdale15@gmail.com>
2018-05-15 07:42:30 +1000

    Truck test updates with mocks

--- src/test/java/com/me4502/supermart/truck/OrdinaryTruckTest.java
+++ src/test/java/com/me4502/supermart/truck/OrdinaryTruckTest.java
@@ -1,104 +1,139 @@
 package com.me4502.supermart.truck;
 
 import static junit.framework.TestCase.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
-import org.junit.Test;
+import java.util.OptionalDouble;
+
+import org.apache.commons.lang3.tuple.ImmutablePair;
 import org.junit.Before;
-/*
-import org.junit.Rule;
-import org.junit.experimental.theories.*;
-import org.junit.runner.RunWith;
-import org.junit.rules.ExpectedException;
-*/
+import org.junit.Test;
 
+import com.google.common.collect.ImmutableSet;
 import com.me4502.supermart.SuperMartApplication;
+import com.me4502.supermart.store.Item;
+import com.me4502.supermart.store.Stock;
 import com.me4502.supermart.truck.OrdinaryTruck;
-import com.me4502.supermart.util.DummyClasses;
 
-public class OrdinaryTruckTest {	
-	DummyClasses dummyClass = new DummyClasses();
+public class OrdinaryTruckTest {
+	// Get the cost of a truck for a certain temperature
+	private double getCost(int quantity) {
+		return (750 + 0.25 * quantity);
+	}
 	
-	//
-	private int stockValidAmount = 1;
-	private double truckValidCost = 750 + 0.25 * stockValidAmount;
-	DummyClasses.StockDummy stockDummyValid = new DummyClasses.StockDummy(stockValidAmount);
+	// Return a stock item -- only need to test for lowest item temperature and total amount
+	private Stock getStock(ImmutableSet<ImmutablePair<Item, Integer>> stockSetAmounts) {
+		Stock stock = mock(Stock.class);
+		when(stock.getStockedItemQuantities()).thenReturn(stockSetAmounts);
+		return stock;
+	}
 	
+	// Valid build parameters
+	int validQuantity = 100;
+	double validCost = getCost(validQuantity);
+	Stock validStock = getStock(ImmutableSet.of(ImmutablePair.of(mock(Item.class), validQuantity)));
 	
-	private int stockInvalidAmount = 1001;
-	private double truckInvalidCost = 750 + 0.25 * stockInvalidAmount;
-	DummyClasses.StockDummy stockDummyInvalid = new DummyClasses.StockDummy(stockValidAmount);	
+	// Return builder for valid item
+	private OrdinaryTruck.OrdinaryBuilder truckBuilder() {
+        return SuperMartApplication.getInstance().getOrdinaryTruckBuilder()
+                .cost(validCost)
+                .cargo(validStock);
+    }
 	
+	// Build the valid item
+    private OrdinaryTruck buildTruck() {
+        return truckBuilder().build();
+    }
+    
+    // Return a built truck with parameters
+    private OrdinaryTruck buildUniqueTruck(double cost, Stock cargo) {
+        return SuperMartApplication.getInstance().getOrdinaryTruckBuilder()
+                .cost(cost)
+                .cargo(cargo)
+                .build();
+    }
+    
+    // Get the supermart instance for testing
 	@Before
     public void setupApplication() {
         new SuperMartApplication();
     }
-
-    private OrdinaryTruck.OrdinaryBuilder truckBuilder() {
-        return SuperMartApplication.getInstance().getOrdinaryTruckBuilder()
-                .cost(truckValidCost)
-                .cargo(stockDummyValid);
-    }
-
-    private OrdinaryTruck buildTruck() {
-        return truckBuilder().build();
-    }
-
+    
+    // Check that the valid truck has build properly
     @Test
     public void testValidBuild() {
-    	buildTruck();
+        OrdinaryTruck OrdinaryTruck = buildTruck();
+        assertEquals(validCost, OrdinaryTruck.getCost());
+        assertEquals(validStock, OrdinaryTruck.getCargo());
     }
 
+    // 
     @Test(expected=IllegalStateException.class)
-    public void testInvalidBuild() {
+    public void testInvalidEmptyBuild() {
         OrdinaryTruck.OrdinaryBuilder builder = SuperMartApplication.getInstance().getOrdinaryTruckBuilder();
         builder.build();
     }
-
-    //@Test(expected=DeliveryException.class)
-    @Test(expected=IllegalStateException.class)
-    public void testInvalidStockBuild() {
-        OrdinaryTruck.OrdinaryBuilder builder = SuperMartApplication.getInstance().getOrdinaryTruckBuilder()
-        		.cost(truckInvalidCost)
-                .cargo(stockDummyInvalid);
-        builder.build();
-    }
-
-/*  
-//    @RunWith(Theories.class)
-//    public static class PrimeTest {
-//    	
-//    @Before
-//	    public void setupApplication() {
-//	        new SuperMartApplication();
-//	    }
-//	@Rule
-//	public ExpectedException thrown = ExpectedException.none();    
-//    	
-//    	
-//        @Theory
-//        public void isPrime(int candidate) {
-//        	DummyClasses.StockDummy stockTester = dummyClass.new StockDummy(candidate); 
-//        	OrdinaryTruck.OrdinaryBuilder builder = SuperMartApplication.getInstance().getOrdinaryTruckBuilder()
-//        			.cost(750 + 0.25 * candidate)
-//        			.cargo(stockTester);
-//        	builder.build();
-//        	thrown.expect(IllegalStateException.class);
-//        }
-//        public static @DataPoints int[] candidates = {1, 2, 3, 4, 5};
-//    }
-*/
     
-    
-    @Test
-    public void testCorrectBuild() {
-        OrdinaryTruck ordinaryTruck = buildTruck();
-        assertEquals(truckValidCost, ordinaryTruck.getCost());
-        assertEquals(stockDummyValid, ordinaryTruck.getCargo());
-    }
-
+    //
     @Test(expected=IllegalStateException.class)
     public void testResetWorks() {
         OrdinaryTruck.OrdinaryBuilder builder = truckBuilder().reset();
         builder.build();
+    }
+
+	// Test values sitting on the lower boundaries
+	@Test
+    public void testOnLowerThresholds() {
+    	// Generate parameters
+		int borderQuantity = 1;
+		double borderCost = getCost(borderQuantity);
+		Stock borderStock = getStock(ImmutableSet.of(ImmutablePair.of(mock(Item.class), borderQuantity)));
+		// Attempt to build
+    	buildUniqueTruck(borderCost, borderStock);
+    }
+
+    // Test values sitting below the lower boundaries
+    @Test(expected=IllegalStateException.class)
+    public void testBelowLowerThresholds() {
+    	// Generate parameters
+		int invalidQuantity = 0;
+		double invalidCost = getCost(invalidQuantity);
+		Stock invalidStock = getStock(ImmutableSet.of(ImmutablePair.of(mock(Item.class), invalidQuantity)));
+		// Attempt to build
+    	buildUniqueTruck(invalidCost, invalidStock);
+    }
+    
+    // Test values sitting on the upper boundaries
+    @Test
+    public void testOnUpperThresholds() {
+    	// Generate parameters
+		int borderQuantity = 1000;
+		double borderCost = getCost(borderQuantity);
+		Stock borderStock = getStock(ImmutableSet.of(ImmutablePair.of(mock(Item.class), borderQuantity)));
+		// Attempt to build
+    	buildUniqueTruck(borderCost, borderStock);
+    }
+    
+    // Test values sitting above the upper boundaries
+    @Test(expected=IllegalStateException.class)
+    public void testAboveUpperThresholds() {
+    	// Generate parameters
+		int invalidQuantity = 1001;
+		double invalidCost = getCost(invalidQuantity);
+		Stock invalidStock = getStock(ImmutableSet.of(ImmutablePair.of(mock(Item.class), invalidQuantity)));
+		// Attempt to build
+    	buildUniqueTruck(invalidCost, invalidStock);
+    }
+    
+    // Test values sitting above the upper boundaries
+    @Test(expected=IllegalStateException.class)
+    public void testNoColdGoods() {
+    	// Generate parameters
+    	Item invalidItem = mock(Item.class);
+    	when(invalidItem.getIdealTemperature()).thenReturn(OptionalDouble.of(10));
+		Stock invalidStock = getStock(ImmutableSet.of(ImmutablePair.of(invalidItem, validQuantity)));
+		// Attempt to build
+    	buildUniqueTruck(validCost, invalidStock);
     }
 }
--- src/test/java/com/me4502/supermart/truck/RefrigeratedTruckTest.java
+++ src/test/java/com/me4502/supermart/truck/RefrigeratedTruckTest.java
@@ -1,97 +1,138 @@
 package com.me4502.supermart.truck;
 
 import static junit.framework.TestCase.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 import java.util.OptionalDouble;
 
+import org.apache.commons.lang3.tuple.ImmutablePair;
 import org.junit.Before;
 import org.junit.Test;
 
+import com.google.common.collect.ImmutableSet;
 import com.me4502.supermart.SuperMartApplication;
+import com.me4502.supermart.store.Item;
+import com.me4502.supermart.store.Stock;
 import com.me4502.supermart.truck.RefrigeratedTruck;
-import com.me4502.supermart.util.DummyClasses;
 
 public class RefrigeratedTruckTest {
-	// This can be used to represent classes without giving them a full implementation
-	DummyClasses dummyClass = new DummyClasses();
+	// Get the cost of a truck for a certain temperature
+	private double getCost(double storageTemperature) {
+		return (900 + 20*Math.pow(Math.E, storageTemperature/5));
+	}
 	
-	//Truck valid
-	private int stockValidAmount = 100;
-	private double stockValidTemp = 0;
-	private double truckValidCost = 750 + Math.pow(Math.E, stockValidTemp/5);
-	DummyClasses.StockDummy stockDummyValid = new DummyClasses.StockDummy(stockValidAmount, OptionalDouble.of(stockValidTemp));
+	// Return an item -- only need to test for item temperature
+	private Item getItem(double storageTemperature) {
+		Item item = mock(Item.class);
+		when(item.getIdealTemperature()).thenReturn(OptionalDouble.of(storageTemperature));
+		return item;
+	}
 	
-	//Truck invalid upper bounds
-	private int stockInvalidUpperAmount = 801;
-	private double stockInvalidUpperTemp = 11;
-	private double truckInvalidUpperCost = 750 + Math.pow(Math.E, stockInvalidUpperTemp/5);
-	DummyClasses.StockDummy stockDummyUpperInvalid = new DummyClasses.StockDummy(stockInvalidUpperAmount, OptionalDouble.of(stockInvalidUpperTemp));	
+	// Return a stock item -- only need to test for lowest item temperature and total amount
+	private Stock getStock(ImmutableSet<ImmutablePair<Item, Integer>> stockSetAmounts) {
+		Stock stock = mock(Stock.class);
+		when(stock.getStockedItemQuantities()).thenReturn(stockSetAmounts);
+		return stock;
+	}
 	
-	//Truck invalid lower bounds
-	private int stockInvalidLowerAmount = 0;
-	private double stockInvalidLowerTemp = -21;
-	private double truckInvalidLowerCost = 750 + Math.pow(Math.E, stockInvalidLowerTemp/5);
-	DummyClasses.StockDummy stockDummyLowerInvalid = new DummyClasses.StockDummy(stockInvalidLowerAmount, OptionalDouble.of(stockInvalidLowerTemp));	
-		
+	// Valid build parameters
+	double validStorageTemp = 1;
+	double validCost = getCost(validStorageTemp);
+	Stock validStock = getStock(ImmutableSet.of(ImmutablePair.of(getItem(validStorageTemp), 100)));
 	
+	// Return builder for valid item
+	private RefrigeratedTruck.RefrigeratedBuilder truckBuilder() {
+        return SuperMartApplication.getInstance().getRefrigeratedTruckBuilder()
+                .cost(validCost)
+                .cargo(validStock)
+                .storageTemperature(validStorageTemp);
+    }
+	
+	// Build the valid item
+    private RefrigeratedTruck buildTruck() {
+        return truckBuilder().build();
+    }
+    
+    // Return a built truck with parameters
+    private RefrigeratedTruck buildUniqueTruck(double cost, Stock cargo, double storageTemperature) {
+        return SuperMartApplication.getInstance().getRefrigeratedTruckBuilder()
+                .cost(cost)
+                .cargo(cargo)
+                .storageTemperature(storageTemperature)
+                .build();
+    }
+    
+    // Get the supermart instance for testing
 	@Before
     public void setupApplication() {
         new SuperMartApplication();
     }
-
-    private RefrigeratedTruck.RefrigeratedBuilder truckBuilder() {
-        return SuperMartApplication.getInstance().getRefrigeratedTruckBuilder()
-                .cost(truckValidCost)
-                .cargo(stockDummyValid)
-                .storageTemperature(stockValidTemp);
-    }
-
-    private RefrigeratedTruck buildTruck() {
-        return truckBuilder().build();
-    }
-
+    
+    // Check that the valid truck has build properly
     @Test
     public void testValidBuild() {
-    	buildTruck();
+        RefrigeratedTruck RefrigeratedTruck = buildTruck();
+        assertEquals(validCost, RefrigeratedTruck.getCost());
+        assertEquals(validStock, RefrigeratedTruck.getCargo());
+        assertEquals(validStorageTemp, RefrigeratedTruck.getStorageTemperature());
     }
 
+    // 
     @Test(expected=IllegalStateException.class)
-    public void testInvalidBuild() {
+    public void testInvalidEmptyBuild() {
         RefrigeratedTruck.RefrigeratedBuilder builder = SuperMartApplication.getInstance().getRefrigeratedTruckBuilder();
         builder.build();
     }
-
-    //@Test(expected=DeliveryException.class)
-    @Test(expected=IllegalStateException.class)
-    public void testInvalidUpperStockBuild() {
-        RefrigeratedTruck.RefrigeratedBuilder builder = SuperMartApplication.getInstance().getRefrigeratedTruckBuilder()
-        		.cost(truckInvalidUpperCost)
-                .cargo(stockDummyUpperInvalid)
-                .storageTemperature(stockInvalidUpperTemp);
-        builder.build();
-    }
     
-    @Test(expected=IllegalStateException.class)
-    public void testInvalidLowerStockBuild() {
-        RefrigeratedTruck.RefrigeratedBuilder builder = SuperMartApplication.getInstance().getRefrigeratedTruckBuilder()
-        		.cost(truckInvalidLowerCost)
-                .cargo(stockDummyLowerInvalid)
-                .storageTemperature(stockInvalidLowerTemp);
-        builder.build();
-    }
-    
-    
-    @Test
-    public void testCorrectBuild() {
-        RefrigeratedTruck RefrigeratedTruck = buildTruck();
-        assertEquals(truckValidCost, RefrigeratedTruck.getCost());
-        assertEquals(stockDummyValid, RefrigeratedTruck.getCargo());
-        assertEquals(stockValidTemp, RefrigeratedTruck.getStorageTemperature());
-    }
-
+    //
     @Test(expected=IllegalStateException.class)
     public void testResetWorks() {
         RefrigeratedTruck.RefrigeratedBuilder builder = truckBuilder().reset();
         builder.build();
+    }
+
+	// Test values sitting on the lower boundaries
+	@Test
+    public void testOnLowerThresholds() {
+    	// Generate bad parameters
+	    double invalidStorageTemp = -20;
+		double invalidCost = getCost(validStorageTemp);
+		Stock invalidStock = getStock(ImmutableSet.of(ImmutablePair.of(getItem(validStorageTemp), 1)));
+		// Attempt to build
+    	buildUniqueTruck(invalidCost, invalidStock, invalidStorageTemp);
+    }
+
+    // Test values sitting below the lower boundaries
+    @Test(expected=IllegalStateException.class)
+    public void testBelowLowerThresholds() {
+    	// Generate bad parameters
+	    double invalidStorageTemp = -21;
+		double invalidCost = getCost(validStorageTemp);
+		Stock invalidStock = getStock(ImmutableSet.of(ImmutablePair.of(getItem(validStorageTemp), 0)));
+		// Attempt to build
+    	buildUniqueTruck(invalidCost, invalidStock, invalidStorageTemp);
+    }
+    
+    // Test values sitting on the upper boundaries
+    @Test
+    public void testOnUpperThresholds() {
+    	// Generate bad parameters
+	    double invalidStorageTemp = 10;
+		double invalidCost = getCost(validStorageTemp);
+		Stock invalidStock = getStock(ImmutableSet.of(ImmutablePair.of(getItem(validStorageTemp), 800)));
+		// Attempt to build
+    	buildUniqueTruck(invalidCost, invalidStock, invalidStorageTemp);
+    }
+    
+    // Test values sitting above the upper boundaries
+    @Test(expected=IllegalStateException.class)
+    public void testAboveUpperThresholds() {
+    	// Generate bad parameters
+	    double invalidStorageTemp = 11;
+		double invalidCost = getCost(validStorageTemp);
+		Stock invalidStock = getStock(ImmutableSet.of(ImmutablePair.of(getItem(validStorageTemp), 801)));
+		// Attempt to build
+    	buildUniqueTruck(invalidCost, invalidStock, invalidStorageTemp);
     }
 }
