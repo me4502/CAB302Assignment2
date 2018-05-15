217363354580c4d7a8e8ee340746b611a4f4faf6
Matthew Miller <mnmiller1@me.com>
2018-05-15 13:24:05 +1000

    Some partial work on the GUI. Needs a decent amount implemented to be fully functional

--- src/main/java/com/me4502/supermart/gui/SuperMartGui.java
+++ src/main/java/com/me4502/supermart/gui/SuperMartGui.java
@@ -1,13 +1,48 @@
 package com.me4502.supermart.gui;
 
+import com.me4502.supermart.SuperMartApplication;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTabbedPane;
+
 /**
- * TODO
+ * Point of entry for the GUI of the SuperMart application.
  *
  * @author Madeline Miller
  */
 public class SuperMartGui {
 
+    private SuperMartApplication application;
+
+    // Form
+    private JFrame frame;
+    private JTabbedPane tabPane;
+
+    // Info pane
+    private JPanel infoPane;
+
+    /**
+     * Creates a new instance of the GUI.
+     */
+    private SuperMartGui() {
+        // Create an application
+        this.application = new SuperMartApplication();
+
+        // Setup form
+        this.frame = new JFrame();
+
+        // Setup info pane
+        this.infoPane = new JPanel();
+
+        this.tabPane = new JTabbedPane();
+        this.tabPane.addTab("Info", this.infoPane);
+        this.frame.add(this.tabPane);
+
+        this.frame.setVisible(true);
+    }
+
     public static void main(String[] args) {
-        throw new UnsupportedOperationException();
+        new SuperMartGui();
     }
 }
