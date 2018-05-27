20f4c092b9b6ae81e1dfbc327e42187a790b3810
Matthew Miller <mnmiller1@me.com>
2018-05-27 15:43:19 +1000

    Improve javadocs and comments in gui

--- src/main/java/com/me4502/supermart/gui/SuperMartGui.java
+++ src/main/java/com/me4502/supermart/gui/SuperMartGui.java
@@ -6,7 +6,6 @@ import com.me4502.supermart.exception.CSVFormatException;
 import com.me4502.supermart.exception.DeliveryException;
 import com.me4502.supermart.exception.StockException;
 import com.me4502.supermart.store.Stock;
-import com.me4502.supermart.store.Store;
 import com.me4502.supermart.store.StoreImpl;
 import com.me4502.supermart.truck.ManifestOptimiser;
 
@@ -35,8 +34,6 @@ import javax.swing.filechooser.FileNameExtensionFilter;
  */
 public class SuperMartGui {
 
-    private Store store;
-
     // Form
     private JFrame frame;
 
@@ -55,7 +52,7 @@ public class SuperMartGui {
     private SuperMartGui() {
         // Create an application and store
         new SuperMartApplication();
-        this.store = new StoreImpl("SuperMart");
+        new StoreImpl("SuperMart");
 
         // Setup form
         this.frame = new JFrame();
@@ -73,6 +70,11 @@ public class SuperMartGui {
         this.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 
+    /**
+     * Creates the info pane of the GUI.
+     *
+     * @return The info panel
+     */
     private JPanel createInfoPane() {
         // Setup info pane
         JPanel infoPane = new JPanel();
@@ -80,6 +82,7 @@ public class SuperMartGui {
         JLabel infoPaneTitle = new JLabel("Store Inventory Management System", SwingConstants.CENTER);
         infoPaneTitle.setFont(new Font("Default", Font.PLAIN, 18));
         infoPane.add(infoPaneTitle);
+
         this.capitalLabel = new JLabel();
         setCapitalLabel();
         infoPane.add(new JLabel("Store: " + StoreImpl.getInstance().getName()));
@@ -88,22 +91,37 @@ public class SuperMartGui {
         return infoPane;
     }
 
+    /**
+     * Updates the capital label with the current capital of the store.
+     */
     private void setCapitalLabel() {
         this.capitalLabel.setText("Capital: " + StoreImpl.getInstance().getFormattedCapital());
     }
 
+    /**
+     * Creates the inventory pane of the GUI.
+     *
+     * @return The inventory pane
+     */
     private JPanel createInventoryPane() {
+        // Setup inventory pane
         JPanel inventoryPane = new JPanel();
         inventoryPane.setLayout(new BoxLayout(inventoryPane, BoxLayout.Y_AXIS));
         JLabel inventoryPaneTitle = new JLabel("Stored Inventory");
         inventoryPaneTitle.setAlignmentX(Component.CENTER_ALIGNMENT);
         inventoryPaneTitle.setFont(new Font("Default", Font.PLAIN, 18));
         inventoryPane.add(inventoryPaneTitle);
+
+        // Create the table that stores the inventory
         this.inventoryTable = new JTable();
         fillInventoryTable(this.inventoryTable);
         inventoryPane.add(new JScrollPane(this.inventoryTable));
+
+        // Setup the buttons on the screen
         JButton loadInventoryButton = new JButton("Load Item Properties");
         JButton loadSalesLogButton = new JButton("Load Sales Log");
+
+        // When the load inventory button is pressed, open the file picker
         loadInventoryButton.addActionListener(e -> {
             JFileChooser fileChooser = new JFileChooser();
             fileChooser.setFileFilter(new FileNameExtensionFilter("CSV Files", "csv"));
@@ -112,6 +130,8 @@ public class SuperMartGui {
                 File file = fileChooser.getSelectedFile();
                 try {
                     CSV.loadItemProperties(file);
+
+                    // Update properties now that it's loaded in
                     optimiseManifests();
                     setCapitalLabel();
                     loadSalesLogButton.setEnabled(true);
@@ -127,6 +147,8 @@ public class SuperMartGui {
                 fillInventoryTable(this.inventoryTable);
             }
         });
+
+        // When the load sales button is pressed, open the file picker
         loadSalesLogButton.setEnabled(false);
         loadSalesLogButton.addActionListener(e -> {
             JFileChooser fileChooser = new JFileChooser();
@@ -136,6 +158,8 @@ public class SuperMartGui {
                 File file = fileChooser.getSelectedFile();
                 try {
                     CSV.loadSalesLog(file);
+
+                    // Update properties now that it's loaded in
                     optimiseManifests();
                     setCapitalLabel();
                     this.saveManifestButton.setEnabled(true);
@@ -148,6 +172,8 @@ public class SuperMartGui {
                 fillInventoryTable(this.inventoryTable);
             }
         });
+
+        // Add the buttons in a panel so they're in the same line
         JPanel buttonPanel = new JPanel();
         buttonPanel.add(loadInventoryButton);
         buttonPanel.add(loadSalesLogButton);
@@ -156,6 +182,11 @@ public class SuperMartGui {
         return inventoryPane;
     }
 
+    /**
+     * Fills a table with inventory data.
+     *
+     * @param inventoryTable The table to fill
+     */
     private void fillInventoryTable(JTable inventoryTable) {
         // Create column names
         String[] columns = {
@@ -165,19 +196,19 @@ public class SuperMartGui {
                 "Sell price ($)",
                 "Reorder point",
                 "Reorder amount",
-                "Temperature (°C)"
+                "Temperature (\u00B0C)"
         };
 
         // Transform the Stock into a data frame for the table model
-        Object[][] data = this.store.getItems().stream()
+        Object[][] data = StoreImpl.getInstance().getItems().stream()
                 .map(item -> new Object[]{
                         item.getName(),
-                        this.store.getInventory().getItemQuantity(item).orElse(0),
+                        StoreImpl.getInstance().getInventory().getItemQuantity(item).orElse(0),
                         item.getManufacturingCost(),
                         item.getSellPrice(),
                         item.getReorderPoint(),
                         item.getReorderAmount(),
-                        item.getIdealTemperature().isPresent() ? item.getIdealTemperature().getAsDouble() : "Dry"
+                        item.getIdealTemperature().isPresent() ? item.getIdealTemperature().getAsDouble() : "N/A"
                 })
                 .toArray(Object[][]::new);
 
@@ -185,18 +216,32 @@ public class SuperMartGui {
         inventoryTable.setModel(new ArrayTableModel(data, columns, false));
     }
 
+    /**
+     * Creates the manifest pane of the GUI.
+     *
+     * @return The manifest pane
+     */
     private JPanel createManifestPane() {
+        // Setup manifest pane
         JPanel manifestPane = new JPanel();
         manifestPane.setLayout(new BoxLayout(manifestPane, BoxLayout.Y_AXIS));
         this.manifestPaneTitle = new JLabel("Manifest");
         this.manifestPaneTitle.setAlignmentX(Component.CENTER_ALIGNMENT);
         this.manifestPaneTitle.setFont(new Font("Default", Font.PLAIN, 18));
         manifestPane.add(this.manifestPaneTitle);
+
+        // Create the table that stores the manifest
         this.manifestTable = new JTable();
         fillManifestTable(this.manifestTable);
         manifestPane.add(new JScrollPane(this.manifestTable));
+
+        // Setup the buttons on the screen
         this.loadManifestButton = new JButton("Load Manifests");
         this.loadManifestButton.setEnabled(false);
+        this.saveManifestButton = new JButton("Save Manifests");
+        this.saveManifestButton.setEnabled(false);
+
+        // When the load button is pressed, open a file picker
         this.loadManifestButton.addActionListener(e -> {
             JFileChooser fileChooser = new JFileChooser();
             fileChooser.setFileFilter(new FileNameExtensionFilter("CSV Files", "csv"));
@@ -205,6 +250,8 @@ public class SuperMartGui {
                 File file = fileChooser.getSelectedFile();
                 try {
                     CSV.loadManifest(file);
+
+                    // Update properties now that it's loaded in
                     setCapitalLabel();
                     this.saveManifestButton.setEnabled(false);
                     this.manifestPaneTitle.setText("Imported Manifest");
@@ -218,8 +265,8 @@ public class SuperMartGui {
                 fillInventoryTable(this.inventoryTable);
             }
         });
-        this.saveManifestButton = new JButton("Save Manifests");
-        this.saveManifestButton.setEnabled(false);
+
+        // When the save button is pressed, open a file picker for saving
         this.saveManifestButton.addActionListener(e -> {
             JFileChooser fileChooser = new JFileChooser();
             fileChooser.setFileFilter(new FileNameExtensionFilter("CSV Files", "csv"));
@@ -234,23 +281,41 @@ public class SuperMartGui {
                 }
             }
         });
+
+        // Add the buttons in a panel so that they're in line
         JPanel buttonPanel = new JPanel();
         buttonPanel.add(this.loadManifestButton);
         buttonPanel.add(this.saveManifestButton);
         manifestPane.add(buttonPanel);
+
         return manifestPane;
     }
 
+    /**
+     * Generates a stock based on items needing re-order, and then creates a manifest.
+     *
+     * @throws DeliveryException If the manifest generation failed
+     */
     private void optimiseManifests() throws DeliveryException {
+        // Create a stock from the items that need restocking
         Stock.Builder stockBuilder = SuperMartApplication.getInstance().getStockBuilder();
-        this.store.getInventory().getStockedItemQuantities().stream()
+        StoreImpl.getInstance().getInventory().getStockedItemQuantities().stream()
                 .filter(pair -> pair.getRight() <= pair.getLeft().getReorderPoint())
                 .forEach(pair -> stockBuilder.addStockedItem(pair.getLeft(), pair.getLeft().getReorderAmount()));
-        this.store.setManifest(new ManifestOptimiser(stockBuilder.build()).getManifest(), false);
+
+        // Set the manifest to the optimised manifest
+        StoreImpl.getInstance().setManifest(new ManifestOptimiser(stockBuilder.build()).getManifest(), false);
+
+        // Update the tables
         this.manifestPaneTitle.setText("Generated Manifest");
         fillManifestTable(this.manifestTable);
     }
 
+    /**
+     * Fills a table with manifest data.
+     *
+     * @param manifestTable The table to fill
+     */
     private void fillManifestTable(JTable manifestTable) {
         // Create column names
         String[] columns = {
@@ -261,7 +326,7 @@ public class SuperMartGui {
         };
 
         // Transform the Manifest into a data frame for the table model
-        Object[][] data = this.store.getManifest().getTrucks().stream()
+        Object[][] data = StoreImpl.getInstance().getManifest().getTrucks().stream()
                 .map(truck -> new Object[]{
                         truck.getType(),
                         truck.getCargoCapacity(),
@@ -274,7 +339,13 @@ public class SuperMartGui {
         manifestTable.setModel(new ArrayTableModel(data, columns, false));
     }
 
+    /**
+     * Entry point of the application
+     *
+     * @param args String args, ignored
+     */
     public static void main(String[] args) {
+        // Create an instance of the GUI
         new SuperMartGui();
     }
 }
